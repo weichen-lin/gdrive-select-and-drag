96588a97df5e3a83a03bc37cd81d946da4576a4f
WeiChen Lin <asdfg09487@gmail.com>
2022-12-29 12:00:10 +0800

    restart build

--- index.tsx
+++
@@ -1,4 +0,0 @@
-import { SelectionArea } from './SelectionArea'
-export { default as VanillaSelectionArea } from '../../vanilla/src/'
-export * from '../../vanilla/src/types'
-export default SelectionArea
